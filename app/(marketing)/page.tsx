ea930a29571a59e74607d1c6815f31a9e2aedd22
alekseytsvetkov <code.aleksey@icloud.com>
2025-07-31 19:37:57 +0300

    update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -133,7 +133,7 @@ export default async function IndexPage() {
         className="container space-y-6 bg-slate-50 dark:bg-transparent"
       >
         <div className="flex max-w-[58rem] flex-col items-start space-y-4 text-left">
-          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+          <h2 className="font-heading text-2xl leading-[1.1] sm:text-2xl md:text-5xl">
             Features
           </h2>
           {/* <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
