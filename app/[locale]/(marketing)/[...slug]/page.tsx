d485486b3cbd0748302c5b968dfbe36cfc8089f5
alekseytsvetkov <code.aleksey@icloud.com>
2025-08-20 12:34:38 +0300

    update

--- app/[locale]/(marketing)/[...slug]/page.tsx
+++ app/[locale]/(marketing)/[...slug]/page.tsx
@@ -93,7 +93,7 @@ export default async function PagePage({ params }: PageProps) {
   return (
     <article className="container py-6 lg:py-12">
       <div className="space-y-4">
-        <h1 className="inline-block font-heading text-4xl lg:text-5xl">
+        <h1 className="inline-block font-heading text-4xl text-primary lg:text-5xl">
           {page.title}
         </h1>
         {page.description && (
