f424af6db0f4c17c6a6d4caa5763f16cb7a3e70b
alekseytsvetkov <code.aleksey@icloud.com>
2025-08-20 12:28:50 +0300

    update

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -80,10 +80,10 @@ const components = {
     />
   ),
   ul: ({ className, ...props }) => (
-    <ul className={cn("my-6 ml-6 list-disc", className)} {...props} />
+    <ul className={cn("my-6 ml-6 list-disc text-primary", className)} {...props} />
   ),
   ol: ({ className, ...props }) => (
-    <ol className={cn("my-6 ml-6 list-decimal", className)} {...props} />
+    <ol className={cn("my-6 ml-6 list-decimal text-primary", className)} {...props} />
   ),
   li: ({ className, ...props }) => (
     <li className={cn("mt-2", className)} {...props} />
