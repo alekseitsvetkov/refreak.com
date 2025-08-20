aa6ca12241f0c92d0c078a16b598f606f3d73b18
alekseytsvetkov <code.aleksey@icloud.com>
2025-08-20 12:15:55 +0300

    update

--- app/[locale]/(marketing)/page.tsx
+++ app/[locale]/(marketing)/page.tsx
@@ -25,7 +25,7 @@ export default async function IndexPage({
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex flex-col items-start gap-4">
           <h1 
-            className="font-heading text-2xl sm:text-4xl md:text-5xl lg:text-5xl"
+            className="font-heading text-2xl text-primary sm:text-4xl md:text-5xl  lg:text-5xl"
             dangerouslySetInnerHTML={{ __html: t("title") }}
           />
           <p 
